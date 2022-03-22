e4e48046af1cbc9e6dfb7bb167c6915f42816235
Amadea Yinata <61137073+ayin2830@users.noreply.github.com>
2022-03-22 21:01:52 +0700

    Create netflix_index.R

---
+++ netflix_index.R
@@ -0,0 +1,36 @@
+install.packages("dplyr")
+library("dplyr")
+
+install.packages("tidyr")
+library("tidyr")
+
+install.packages("lubridate")
+library("lubridate")
+
+install.packages('ggplot2')
+library("ggplot2")
+
+
+combined_ds <- inner_join(`2020_exchange_rates`,netflix_data,by=c("Country"))
+View(combined_ds)
+
+US_price <- 13.99
+
+netflix_index <- function(x) {
+  return((x-13.99)/13.99)
+}
+
+nf_index = (combined_ds$Cpm_standard-US_price)/US_price
+View(nf_index)
+
+for_100 =  167.88/(combined_ds$Cpm_standard)
+
+combined_ds <- combined_ds %>% 
+  mutate(netflix_index = nf_index*100,for_100_usd = for_100)
+
+
+
+write.csv(combined_ds,"/Users/yinta/Desktop/netflix_index_upd.csv", row.names = FALSE)
+
+
+ 
